68d5be6d2379499ffc33bb51d6263d4f7ee43bfd
gatari <zavierspamstuff@gmail.com>
2024-02-01 21:17:24 +0800

    Update root.go

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -6,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gocheck",
 	Short: "A Golang CLI tool to test the evasiveness of your malware.",
@@ -32,8 +31,6 @@ A tool to identify byte chunks that are flagged by AMSI or Windows Defender.
 	`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
