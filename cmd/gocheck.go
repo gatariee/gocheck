c7d05c1bee27ccea381a52dd5c3b2e381e3722d5
gatari <zavierspamstuff@gmail.com>
2024-02-04 12:54:02 +0800

    oops

--- cmd/gocheck.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import cli "github.com/gatariee/gocheck/cmd/cli"
-
-func main() {
-	cli.Execute()
-}
---
+++ gocheck.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gatariee/gocheck/cmd"
+
+func main() {
+	cmd.Execute()
+}
